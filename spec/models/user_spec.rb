f0f66ea9c0b3a1e090b6d3f632a282ae4737c9aa
locomotif18 <marcelo.vitolo@gmail.com>
2013-01-26 20:59:04 -0500

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,7 +15,7 @@ describe User do
 
   before do
     @user = User.new(name: "Example User", email: "user@example.com",
-    				 password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
   end
   
   subject { @user }
@@ -26,20 +26,31 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
-  
+
   it { should be_valid }
-  
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
-  
+
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
   end
-  
+
   describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
@@ -52,7 +63,7 @@ describe User do
       addresses.each do |invalid_address|
         @user.email = invalid_address
         @user.should_not be_valid
-      end      
+      end
     end
   end
 
@@ -62,7 +73,7 @@ describe User do
       addresses.each do |valid_address|
         @user.email = valid_address
         @user.should be_valid
-      end      
+      end
     end
   end
 
